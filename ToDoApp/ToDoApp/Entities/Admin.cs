907bbea6e23451f8a1aece47e5967636163d8086
nyazadzhiev <nyazadzhiev@gmail.com>
2021-10-24 17:16:17 +0300

    Added Entities

---
+++ ToDoApp/ToDoApp/Entities/Admin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Entities
+{
+    public class Admin : Person
+    {
+
+    }
+}
---
+++ ToDoApp/ToDoApp/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ToDoApp.Entities
+{
+    class User : Person
+    {
+        public List<TaskList> ToDoList { get; set; }
+        public List<TaskList> SharedToDoList { get; set; }
+        public Admin Creator { get; set; }
+
+        public User()
+        {
+            ToDoList = new List<TaskList>();
+            SharedToDoList = new List<TaskList>();
+        }
+    }
+}
